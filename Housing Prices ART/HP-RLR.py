a499fc3fc444390dc7cf26c8f682d637da71d29b
Somin Wadhwa <sominwadhwa@gmail.com>
2016-10-29 14:09:58 +0530

    Fixed categorical issues
    Categorical issues were occurring as Test & Training data were being
    processed differently. Fixed by concatenation.
    
    Still remains to be seen how it affects overfitting

--- Housing Prices ART/HP-RLR.py
+++ Housing Prices ART/HP-RLR.py
@@ -25,7 +25,9 @@ import xgboost as xgb
 train_DF = pd.read_csv('train.csv')
 test_DF = pd.read_csv('test.csv')
 train_DF.head()
-
+concat = pd.concat((train_DF.loc[:,'MSSubClass':'SaleCondition']
+                      ,test_DF.loc[:,'MSSubClass':'SaleCondition'])) #Avoid Categorical differences ahead
+                      
 #Plot a general distribution of the prices (Fixing the skewed meytSkewed Metric)
 figure, (ax1, ax2) = plt.subplots(1,2,figsize = (17,5))
 sns.distplot(train_DF['SalePrice'], kde = False, ax = ax1, bins = 100)
@@ -33,34 +35,27 @@ sns.distplot(np.log1p(train_DF["SalePrice"]), kde = False, axlabel = 'Normalized
 train_DF['SalePrice'] = np.log1p(train_DF["SalePrice"])
 
 #fixing other numeric skewed metrics
-numeric_features_train = train_DF.dtypes[train_DF.dtypes != 'object'].index
-numeric_features_test = test_DF.dtypes[train_DF.dtypes != 'object'].index
+numeric_features_train = concat.dtypes[train_DF.dtypes != 'object'].index
 
-skewed_features_train = train_DF[numeric_features_train].apply(lambda x: skew(x))
-skewed_features_test = test_DF[numeric_features_test].apply(lambda x: skew(x))
+skewed_features_train = concat[numeric_features_train].apply(lambda x: skew(x.dropna()))
 
 skewed_features_train = skewed_features_train[skewed_features_train > 0.75]
-skewed_features_test = skewed_features_test[skewed_features_test > 0.75]
 skewed_features_train = skewed_features_train.index
-skewed_features_test = skewed_features_test.index
 
-train_DF[skewed_features_train] = np.log1p(train_DF[skewed_features_train])
-test_DF[skewed_features_test] = np.log1p(test_DF[skewed_features_test])
+concat[skewed_features_train] = np.log1p(concat[skewed_features_train])
 
-train_DF.head()
+concat.head()
 
 #Getting dummies for all the non numeric data
-train_DF = pd.get_dummies(train_DF)
-test_DF = pd.get_dummies(test_DF)
-
+concat = pd.get_dummies(concat)
 #Fill in empty values with mean of each column
-train_DF = train_DF.fillna(train_DF.mean())
-test_DF = test_DF.fillna(test_DF.mean())
+concat = concat.fillna(train_DF.mean())
 
 #Classifying training and test data
-X = train_DF.drop(['Id','SalePrice'], axis = 1, inplace = False)
-Y = train_DF['SalePrice']
-X_test = test_DF.drop('Id', axis = 1, inplace = False)
+X = concat[:train_DF.shape[0]]
+X_test = concat[train_DF.shape[0]:]
+Y = train_DF.SalePrice
+X.head(3)
 
 #Define function for determining Tuning Parameter
 def cv_error(model):
@@ -116,3 +111,7 @@ preds["Difference"] = preds["true"] - preds["Predicted"]
 preds.plot(x = "Predicted", y = "Difference",kind = "scatter", title = "Residual Features")
 
 print (ridgeReg.score(X,Y))
+
+preds = np.expm1(ridgeReg.predict(X_test)) #Exponential function used to balance out log(x + 1) 
+solution = pd.DataFrame({"id":test_DF.Id, "SalePrice":preds})
+solution.to_csv("ridge_sol.csv", index = False)
